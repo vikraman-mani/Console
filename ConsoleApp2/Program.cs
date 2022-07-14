0f501044444f3b12405e45690a453f011a3ae39f
vikraman-mani <vikraman02032001@gmail.com>
2022-07-14 09:22:24 +0530

    Console App 2 added

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(5,"RED");
+            c1.Area();
+            c1.ShowColor();
+            Cylinder r = new Cylinder(2,5,"Yellow"); 
+            r.Area();
+            r.ShowColor();
+        }
+    }
+}
---
+++ ConsoleApp2/Shape.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    abstract class Shape
+    {
+        public abstract double Area();
+        public String Color { get; set; }
+        public Shape(String color)
+        {
+            Color = color;
+        }
+        public void ShowColor()
+        {
+            Console.WriteLine("Color of the shape is : " + Color);
+        }
+    }
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double rad, String color) : base(color)
+        {
+            Radius = rad;
+        }
+        public override double Area()
+        {
+            double area = Math.PI * Radius * Radius;
+            return area;
+        }
+    }
+    class Cylinder : Circle
+    {
+        public double Height { set; get; }
+        public Cylinder(double rad, double height, String color) : base(rad, color)
+        {
+            this.Height = height;
+        }
+        public override double Area()
+        {
+            //double area = 2 * (Math.PI * radius * radius + Math.PI * radius *  height);
+            double A = 2 * Math.PI * Radius * (Height + Radius);
+            return A;
+        }
+    }
+}
